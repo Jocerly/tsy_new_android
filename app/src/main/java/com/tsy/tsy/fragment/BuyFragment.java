b0971bc9144b534002c41e083a36019794fd6da1
Jocerly <Jocerly198835>
2018-04-17 15:47:24 +0800

    框架搭建，加入上啦加载更多，下拉刷新

--- app/src/main/java/com/tsy/tsy/fragment/BuyFragment.java
+++ app/src/main/java/com/tsy/tsy/fragment/BuyFragment.java
@@ -5,20 +5,38 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.tsy.tsy.BaseFragment;
 import com.tsy.tsy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Jocerly on 2018/4/16.
  */
 
-public class BuyFragment extends BaseFragment{
+public class BuyFragment extends BaseFragment {
+
+
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
+    Unbinder unbinder;
+    @BindView(R.id.text)
+    TextView text;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_buy, null);
         aty = getActivity();
+        unbinder = ButterKnife.bind(this, view);
         initView(view);
         initData(view);
         return view;
@@ -27,10 +45,31 @@ public class BuyFragment extends BaseFragment{
     @Override
     public void initView(View view) {
 
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadmore(1000, true);
+                text.setText("刷新成功");
+            }
+        });
+
+        refreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadmore(1000, true);
+                text.setText("加载成功");
+            }
+        });
     }
 
     @Override
     public void initData(View view) {
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/com/tsy/tsy/fragment/MemberCenterFragment.java
+++ app/src/main/java/com/tsy/tsy/fragment/MemberCenterFragment.java
@@ -2,25 +2,47 @@ package com.tsy.tsy.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.tsy.tsy.BaseFragment;
 import com.tsy.tsy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Jocerly on 2018/4/16.
  */
 
 public class MemberCenterFragment extends BaseFragment {
-
+    Unbinder unbinder;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mine, null);
         aty = getActivity();
+        unbinder = ButterKnife.bind(this, view);
         initView(view);
         initData(view);
         return view;
@@ -28,11 +50,28 @@ public class MemberCenterFragment extends BaseFragment {
 
     @Override
     public void initView(View view) {
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishRefresh(1000, true);
+            }
+        });
+        refreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadmore(1000, true);
+            }
+        });
 
     }
 
     @Override
     public void initData(View view) {
+    }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
